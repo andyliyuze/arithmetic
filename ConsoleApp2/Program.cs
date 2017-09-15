3ad700e752dc5a153d46c6b872a1afd9aa62bf2a
andyliyuze <874787504@qq.com>
2017-09-15 16:54:30 +0800

    添加项目文件。

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+ 
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //求两个有序数组的交集
+
+            //Hashtable解法，将其中一个放入hash表
+            Hashtable ht = new Hashtable
+            {
+                { Guid.NewGuid(), 1 }, //添加keyvalue键值对
+                { Guid.NewGuid(), 3},
+                { Guid.NewGuid(), 4 },
+                { Guid.NewGuid(),6 }
+            }; //创建一个Hashtable实例
+
+            int[] arr = { 3, 4, 5, 6, 7, 10, 15, 50, 51, 52, 54, 55 };
+            int[] arr2 = { 1, 2, 3, 4, 5, 9, 10, 11, 12, 50, 51, 52, 54 };
+
+            LinkedList<int> list = new LinkedList<int>();
+            int j = 0;
+            foreach (int i in arr)
+            {
+                if (ht.ContainsValue(i))
+                {
+                    list.AddLast(i);
+
+                }
+                j++;
+
+            }
+            Console.WriteLine("时间复杂度：" + j + "");
+            j = 0;
+            list.Clear();
+            int arrIndex = 0;
+            int arr2Index = 0;
+            while (arrIndex < arr.Length && arr2Index < arr2.Length)
+            {
+                if (arr[arrIndex] < arr2[arr2Index])
+                {
+
+                    arrIndex++;
+                }
+                else if (arr2[arr2Index] < arr[arrIndex])
+                {
+
+                    arr2Index++;
+                }
+                else
+                {
+                    list.AddLast(arr[arrIndex]);
+                    arrIndex++;
+                    arr2Index++;
+
+                }
+                j++;
+            }
+
+            j = 0;
+            list.Clear();
+            arrIndex = 0;
+            arr2Index = 0;
+            //索引递进法，双方数组从0处开始对比，相同就是交集，小的一方索引进1
+            while (arrIndex < arr.Length && arr2Index < arr2.Length)
+            {
+                if (arr[arrIndex] < arr2[arr2Index])
+                {
+
+                    //  list.AddLast(arr[arrIndex]);
+                    arrIndex++;
+                }
+                else if (arr2[arr2Index] < arr[arrIndex])
+                {
+
+                    //list.AddLast(arr2[arr2Index]);
+                    arr2Index++;
+                }
+                else
+                {
+                    list.AddLast(arr[arrIndex]);
+                    arrIndex++;
+                    arr2Index++;
+
+                }
+                j++;
+                //   double k = (double)5 / (double)2;  //‘/’取商，“%”取余
+
+            }
+
+            Console.WriteLine("时间复杂度：" + j + "");
+
+
+            int key = 3;
+            j = 0;
+
+            //求有序数组的指定元素
+            //二分法查找
+            int beginIndex = 0;
+            int endIndex = arr.Length - 1;
+            int midIndex = (beginIndex + beginIndex) / 2;
+            while (beginIndex <= endIndex)
+            {
+                midIndex = (beginIndex + endIndex) / 2;
+                var value = arr[midIndex];
+                if (value < key)
+                {
+                    beginIndex = midIndex + 1;
+
+
+                }
+                else if (value > key)
+                {
+
+                    endIndex = midIndex - 1;
+
+                }
+                else
+                {
+                    Console.WriteLine("找到了在：" + midIndex);
+                    endIndex = -1;
+                }
+                j++;
+            }
+            Console.WriteLine("时间复杂度：" + j + "");
+
+
+
+
+            //利用两个栈是，实现队列
+
+            Equeue equeue = new Equeue();
+            equeue.EnQueue(5);
+            equeue.EnQueue(6);
+            equeue.EnQueue(7);
+             var o= equeue.DeQueue();
+            o = equeue.DeQueue();
+            equeue.EnQueue(8);
+            o = equeue.DeQueue();
+            Console.WriteLine("Hello World!");
+
+
+            int[] array = { 49, 38, 65, 97, 76, 13, 27 ,2,1,100,11,50,49,65};
+          int[] array2 = { 69,39,55,100,78,22,99 };
+            QuickSort.sort(array2, 0, array2.Length - 1);
+            Console.ReadLine();
+          
+        }
+    }
+
+    //只利用两个栈，实现一个队列
+
+    //入队时，将元素压入s1。
+
+   // 出队时，判断s2是否为空，如不为空，则直接弹出顶元素；如为空，则将s1的元素逐个“倒入”s2，把最后一个元素弹出并出队。
+
+    class Equeue
+    {
+        public Equeue()
+        {
+
+            s1 = new Stack();
+            s2 = new Stack();
+        }
+
+        private Stack s1;
+        private Stack s2;
+
+
+        public void EnQueue(object o)
+        {
+          
+            s1.Push(o);
+        }
+        public object DeQueue()
+        {
+ 
+            if (s2.Count != 0) { return s2.Pop(); }
+            else
+            {
+                while (s1.Count > 0)
+                {
+                    var item = s1.Pop();
+                    s2.Push(item);
+                }
+                return  s2.Pop();
+            }
+        }
+    }
+}
---
+++ ConsoleApp2/QuickSort.cs
@@ -0,0 +1,70 @@
+﻿
+using System;
+namespace ConsoleApp2
+{
+   public class QuickSort
+    {
+       
+        /**一次排序单元，完成此方法，key左边都比key小，key右边都比key大。
+          
+ 
+        **@param array排序数组 
+          
+ 
+        **@param low排序起始位置 
+          
+ 
+        **@param high排序结束位置
+          
+ 
+        **@return单元排序后的数组 */
+        private static int sortUnit(int[] array, int low, int high)
+        {
+            int key = array[low];
+            while (low < high)
+            {
+                /*从后向前搜索比key小的值*/
+                while (array[high] >= key && high > low)
+                {
+                    --high;
+                }
+                /*比key小的放左边*/
+                array[low] = array[high];
+                /*从前向后搜索比key大的值，比key大的放右边*/
+                while (array[low] <= key && high > low)
+                {
+                    ++low;
+                }
+                /*比key大的放右边*/
+                array[high] = array[low];
+            }
+            /*左边都比key小，右边都比key大。//将key放在游标当前位置。//此时low等于high */
+            array[low] = key;
+            foreach (int i in array)
+            {
+                Console.Write("{0}\t", i);
+            }
+            Console.WriteLine();
+       
+            return high;
+        }
+        /**快速排序 
+        *@paramarry 
+        *@return */
+        public static void sort(int[] array, int low, int high)
+        {
+            //因为数组是引用类型，所以不用返回新的数组
+            //以下算法是先对对比数左边进行递归遍历，对左边排序，然后在对右边递归，右边排序
+            //递归结束条件是左边下标大于右边下边
+
+            if (low >= high)
+                return;
+            /*完成一次单元排序*/
+            int index = sortUnit(array, low, high);
+            /*对左边单元进行排序*/
+            sort(array, low, index - 1);
+            /*对右边单元进行排序*/
+            sort(array, index + 1, high);
+        }
+    }
+}
